d247a30f79c483409850efcb8678f02835c2d69f
Dima <Dmitrijsnaudzuns@gmail.com>
2023-11-19 01:53:07 -0500

    new projs

--- src/App.vue
+++ src/App.vue
@@ -79,7 +79,43 @@ export default {
             </h4>
             <v-img :src="`dashboard.jpg`" class="mt-1" />
           </v-carousel-item> 
-        <v-carousel-item>
+          <v-carousel-item>
+            <h2>
+                Skating Environment Reinforcement Learning
+            </h2>
+            <h4>
+              Implemented a SARSA, Q-Learning, and Double Q-Learning Agent to solve a 4x4 grid world environment to achieve maximum points.
+              </h4>
+            <v-img :src="`skating.jpg`" class="mt-1"/>
+          </v-carousel-item>
+          <v-carousel-item>
+            <h2>
+                8-Puzzle Problem
+            </h2>
+            <h4>
+              Solved the 8-Puzzle Problem with BFS, DFS, Greedy Search, and A* Search to compare runtimes and solutions between Algorithms. 
+              </h4>
+            <v-img :src="`sliding.jpg`" class="mt-1"/>
+          </v-carousel-item>
+          <v-carousel-item>
+            <h2>
+                Multi-Pool Allocator
+            </h2>
+            <h4>
+              Implemented a multi-pool allocator to allocatem memory from the OS capable of running Vim!
+              </h4>
+            <v-img :src="`malloc.jpg`" class="mt-1"/>
+          </v-carousel-item>
+          <v-carousel-item>
+            <h2>
+                Instant Messenger
+            </h2>
+            <h4>
+              Created packet encoding and decoding for a basic instant messaging protocol.
+            </h4>
+            <v-img :src="`messenger.jpg`" class="mt-1"/>
+          </v-carousel-item>
+          <v-carousel-item>
             <h2>
               <a href="https://devpost.com/software/quantify-6lxpzo" class="text-decoration-underline">
                 UB Hacking Fall 2022 - Quantify
